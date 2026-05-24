1d40f106c06e3659af3bb29ec61707200bf6e517
Dan_foodz <thatdanielwinn@proton.me>
2026-05-23 20:43:19 -0500

    ImgView: Added BMP and ICO support

--- js/imgview.js
+++ js/imgview.js
@@ -9,24 +9,30 @@ const fileInput = document.getElementById('imageUploader');
 const canvas = document.getElementById('image');
 const ctx = canvas.getContext('2d');
 
-const validformats = ["PNG", "JPEG", "GIF", "WEBP"]
+const validformats = ["PNG", "JPEG", "GIF", "WEBP", "BMP", "ICO"]
 const formatcolors = {
     "PNG": "#164a6e",
     "JPEG": "#444d52",
     "GIF": "#7f4c1e",
     "WEBP": "#396311",
+    "BMP": "#943f40",
+    "ICO": "#4e2b9d",
 }
 const formatlogos = {
     "PNG": "/content/IF_PNG_S.png",
     "JPEG": "/content/IF_JPEG_S.png",
     "GIF": "/content/IF_GIF_S.png",
     "WEBP": "/content/IF_WEBP_S.png",
+    "BMP": "/content/IF_BMP_S.png",
+    "ICO": "/content/IF_ICO_S.png",
 }
 const compmethods = {
     "PNG": "Lossless (DEFLATE)",
     "JPEG": "Lossy (JPEG Compression)",
     "GIF": "Lossless (LZW)",
     "WEBP": "Unknown (Lossy or Lossless)",
+    "BMP": "Lossless (RLE)",
+    "ICO": "Lossless (BMP or PNG)",
 }
 
 const formatborders = document.getElementsByClassName("formatborder")
@@ -60,13 +66,14 @@ fileInput.addEventListener('change', () => {
         document.getElementById("filename").innerText = fileInput.value.split("\\")[fileInput.value.split("\\").length-1];
 
         // Load Format Info
-        if (validformats.includes(file.type.split("/")[1].toUpperCase())) {
-            updateformatcolor(file.type.split("/")[1].toUpperCase())
-            updateformatlogo(file.type.split("/")[1].toUpperCase())
+        const fileformat = fileInput.value.split("\\")[fileInput.value.split("\\").length-1].split(".")[fileInput.value.split("\\")[fileInput.value.split("\\").length-1].split(".").length-1].toUpperCase()
+        if (validformats.includes(fileformat)) {
+            updateformatcolor(fileformat)
+            updateformatlogo(fileformat)
 
-            document.getElementById("formatname").innerText = file.type.split("/")[1].toUpperCase();
+            document.getElementById("formatname").innerText = fileformat;
             document.getElementById("mimetype").innerText = file.type;
-            document.getElementById("compmethod").innerText = compmethods[file.type.split("/")[1].toUpperCase()];
+            document.getElementById("compmethod").innerText = compmethods[fileformat];
         } else {
             document.getElementById("formatname").innerText = "---";
             document.getElementById("mimetype").innerText = "---";
